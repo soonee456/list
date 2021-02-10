de5b3693ef844da239df4c5c12dfd84e25a237ed
soonee456 <soonee456@gmail.com>
2021-02-11 00:29:55 +0900

    basic form

---
+++ common.js
@@ -0,0 +1,19 @@
+$(function(){
+    //slider
+    $(".btn_source").click(function(){  
+        $(this).parents(".template").toggleClass("on").find(".source").slideToggle(200);              
+    });
+
+    //메뉴 복사
+    $(".btn_copy").click(function(){
+        var $temp = $("<textarea>");
+        var copyTxt = '';
+        
+        $("body").append($temp);
+        copyTxt = $(this).parents(".template").find("textarea").val();
+        $temp.val(copyTxt).select();
+        document.execCommand("copy");
+        $temp.remove();	 
+        alert('Copy Done!');
+    });
+});
